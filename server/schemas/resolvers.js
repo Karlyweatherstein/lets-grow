f94d157cc75e273781e481aa85bacc54d5ebee46
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-06-29 20:36:10 -0400

    stripe structure, category.js model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { Category, Order, Product, User } = require("../models");
 const { signToken } = require("../utils/auth");
+const stripe = require('stripe')('api key here');
 
 const resolver = {
   Query: {
@@ -52,8 +53,41 @@ const resolver = {
       throw new AuthenticationError("Not logged in");
     },
     checkout: async (parent, args, context) => {
-      // stripe intergration?
-    },
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const line_items = [];
+
+      const { products } = await order.populate('products');
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.crate({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${url}/images${products[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
   },
   Mutation: {
     addUser: async (parent, args) => {
