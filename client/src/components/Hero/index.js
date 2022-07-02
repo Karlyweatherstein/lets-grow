03d185580acebbde072d3f0e106bbdfa6ad9e514
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-07-01 20:38:05 -0400

    updated hero

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,36 +1,48 @@
 import React from "react";
+import Select from "react-select";
 
 function Hero() {
-  function dropFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  window.onclick = function (event) {
-    if (!event.target.matches(".dropbtn")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-          openDropdown.classList.remove("show");
-        }
-      }
-    }
-  };
+  const categoryList = [
+    {
+      value: 1,
+      label: "Fitness",
+    },
+    {
+      value: 2,
+      label: "Nutrition",
+    },
+    {
+      value: 3,
+      label: "The Arts",
+    },
+    {
+      value: 4,
+      label: "School",
+    },
+    {
+      value: 5,
+      label: "Marketing",
+    },
+  ];
 
   return (
     <div className="hero">
       <h1 className="titleFonts">Let's Grow!</h1>
-      <div className="dropdown">
-        <button onClick="dropFunction" className="dropdownBtn paragraphFonts">
-          Select a topic
-        </button>
-        <div className="dropOptions paragraphFonts">
-          <a href="#">Fitness</a>
-          <a href="#">Nutrition</a>
-          <a href="#">The arts</a>
+      <div>
+        <button>Select a topic</button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="/fitness">
+            Fitness
+          </a>
+          <a class="dropdown-item" href="/nutrition">
+            Nutrition
+          </a>
+          <a class="dropdown-item" href="/arts">
+            The arts
+          </a>
         </div>
       </div>
+
       <p className="paragraphFonts">
         Which community are you wanting to grow in? Select a category above for
         a list of our trainers!
